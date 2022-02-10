9413169fcd071f0f852182b7dc300058f2cf71e0
Valentina <tibaldo.valentina@gmail.com>
2022-02-10 09:09:38 +0100

    fixin linkage issues

--- include/utils.h
+++ include/utils.h
@@ -2,17 +2,13 @@
 #define utils_h
 
 #include <string>
+#define XXH_INLINE_ALL
+
 #include "xxhash.h"
 
 
 // Example for a function which hashes a null terminated string with XXH32().
-XXH64_hash_t hash_string(const char* _string, XXH64_hash_t seed)
-{
-    // NULL pointers are only valid if the length is zero
-    size_t length = (_string == NULL) ? 0 : strlen(_string);
-    return XXH64((void*)_string, length, seed);
-}
-
+XXH64_hash_t hash_string(const char* _string, XXH64_hash_t seed);
 
 
 #endif /* utils_h */
---
+++ src/utils.cpp
@@ -0,0 +1,15 @@
+//
+//  utils.cpp
+//  distrains
+//
+//  Created by vale on 09/02/22.
+//
+
+#include <stdio.h>
+#include "utils.h"
+
+XXH64_hash_t hash_string(const char* _string, XXH64_hash_t seed){
+    // NULL pointers are only valid if the length is zero
+    size_t length = (_string == NULL) ? 0 : strlen(_string);
+    return XXH64((void*)_string, length, seed);
+}
